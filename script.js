3b3c3ee62aac8fb91ab16e7a3e44e51d40528b8f
Thi <114497724+ThiYum@users.noreply.github.com>
2024-05-28 19:43:42 -0300

    commit

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const caixaPerguntas = document.querySelector ('.caixa-perguntas');
 const caixaAlternativas = document.querySelector ('.caixa-alternativas');
 const caixaResultado = document.querySelector ('.caixa-resultado');
 const textoResultado = document.querySelector ('.texto-resultado');
+
