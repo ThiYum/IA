92607d080bca22276ccb3d4b0d08086f1d36bf82
Thi <114497724+ThiYum@users.noreply.github.com>
2024-05-28 20:18:24 -0300

    commit

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ const textoResultado = document.querySelector ('.texto-resultado');
 
 const perguntas = [
     {
-        enunciado: "Você abre seus olhos. A primeira coisa que vê, ainda meio borrada pelos olhos sonolentos são árvores, de madeira escura e folhas azuladas. O que você faz?",
+        enunciado: "Você abre seus olhos. A primeira coisa que vê, ainda meio borrado por estar sonolento são árvores, de madeira escura e folhas azuladas. O que você faz?",
         alternativas: [
             "Volto a dormir.",
             "Olho em volta."
         ]
     },
     {
-        enunciado: "pergunta 2",
+        enunciado: "Você olha em volta, observando a floresta, iluminada somente pelos pequenos peixinhos voando à sua volta. À sua direita, há um lago de onde esses peixinhos iluminados em tons de azul e branco estão emergindo; Já à sua direita, você repara que atrás de alguns arbusto há galhos brilhantes.",
         alternativas: [
-            "alternativa 1",
-            "alternativa 2"
+            "Inspecionar arbusto",
+            "Ir em direção ao lago",
+            "Voltar a dormir."
         ]
     },
     {
-        enunciado: "pergunta 3",
+        enunciado: "Ao se aproximar dos arbustos, os galhos se mechem em um movimento rápido. Emergindo dos arbustos, um cervo iluminado aparece rapidamente, te assustando. Você dá um leve salto para trás, assustando também o cervo que corre na direção oposta. O cervo para e olha pra você mais uma vez e então segue por um caminho dourado em meio a árvores convenientemente curvadas como um arco.",
         alternativas: [
-            "alternativa 1",
-            "alternativa 2"
+            "Seguir o cervo.",
+            "Voltar para o lago."
         ]
     },
     {
